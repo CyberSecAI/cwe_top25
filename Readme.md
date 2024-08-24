d219693a96a48164808165ed28d7826273b95fbf
Crashedmind <crashedmind@gmail.com>
2024-08-24 17:15:45 +0100

    add desc

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+# Overview
+1. Do EDA on Top 25 CWEs file from MITRE
+2. Add CVE Descriptions to Top 25
+3. Remove Notes
+
+## Do EDA on Top 25 CWEs file from MITRE
+
+top25_eda.ipynb
+
+
+## Add CVE Descriptions to Top 25
+
+nvd/CVSSData.ipynb
+top25_add_cve_desc.ipynb
+
+## Remove Notes
---
+++ nvd/get_nvd_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+
+mkdir -p jsondata
+cd jsondata
+rm *.json 
+rm *.zip 
+
+# The following command / expansion may not work in all environments 
+# wget https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-{2021..2022}.json.zip 
+# so an alternative is  given here
+
+
+# Base URL for the NIST NVD data
+base_url="https://nvd.nist.gov/feeds/json/cve/1.1/"
+
+# Define the range of years 
+start_year=2021
+end_year=2022
+
+# Loop through the years and download files for each year
+for ((year=start_year; year<=end_year; year++)); do
+    file_name="nvdcve-1.1-${year}.json.zip"
+    url="${base_url}${file_name}"
+    
+    # Use wget to download the files to dir jsondata
+    wget "$url" 
+    
+done
+
+unzip -o "*.zip"
+
+
+date > ../date.txt
